920b220759ead43a0fa8080672a63a4219a28e10
Ejjonzey <ejjonzey1982@gmail.com>
2017-08-30 12:07:55 -0400

    post model made

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170830143229) do
+ActiveRecord::Schema.define(version: 20170830160322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,14 @@ ActiveRecord::Schema.define(version: 20170830143229) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "synopsis"
+    t.bigint "city_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["city_id"], name: "index_posts_on_city_id"
+  end
+
+  add_foreign_key "posts", "cities"
 end
