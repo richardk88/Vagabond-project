fad0ba205036334567eb17b7a31d2ee1fa92e3fd
richardk88 <richardk88@hotmail.com>
2017-08-31 08:53:05 -0400

    more styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,11 @@ City.create(
     name: "Atlanta",
     picture: "https://media.bizj.us/view/img/3972271/skyline-of-atlanta*750xx1600-900-0-1384.jpg"
 )
+City.create(
+    name: "Miami",
+    picture: "https://media.local10.com/weather/icons/wx_66.jpg"
+)
+City.create(
+    name: "New York",
+    picture: "https://www.gentlegiant.com/wp-content/uploads/2015/06/New-York.jpg"
+)
